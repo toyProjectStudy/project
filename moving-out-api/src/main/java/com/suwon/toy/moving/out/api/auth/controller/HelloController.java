e055769b8e6ef6a16f68e4294a15df77f073662e
tkfkd249 <thgus249@naver.com>
2021-09-08 22:18:05 +0900

    Revert "?"
    This reverts commit 9b3990e4b8edbdf09f9bbbddb46de013e948da53.

--- moving-out-api/src/main/java/com/suwon/toy/moving/out/api/auth/controller/HelloController.java
+++ moving-out-api/src/main/java/com/suwon/toy/moving/out/api/auth/controller/HelloController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class HelloController {
-	
 
     @GetMapping("/main")
     public ResponseEntity<String> hello() {
